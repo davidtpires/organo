a79401492f3b9afc992d1aa88cb3f56877f992d2
David T. <davidt.info@gmail.com>
2025-03-02 12:06:43 -0300

    Olá mundo!

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
+        <p>Bem-vindo ao React!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
