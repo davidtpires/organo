0e324795b732e1dfa067c54e62e5bc91ef1839be
David T. <davidt.info@gmail.com>
2025-03-05 20:12:39 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'> 
+        <div className='cabecalho'>
+            <img src='https://github.com/viniciosneves.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>Vinicios Neves</h4>
+            <h5>Instrutor</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
