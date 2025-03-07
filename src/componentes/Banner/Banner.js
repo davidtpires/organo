e91b09cd9b74b16e5f9fa26572ec26daa64eb1c9
David T. <davidt.info@gmail.com>
2025-03-07 16:42:23 -0300

    Depurando código

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,10 +1,15 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 export const Banner = () => {
     //JSX
-    return(
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-        </header>
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+            </header>
+            <h1>Teste</h1>
+        </>
+
     )
 }
