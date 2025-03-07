47f2ffe0fed73cd70971073fc7133139eab9d73b
David T. <davidt.info@gmail.com>
2025-03-07 15:24:45 -0300

    Nem tudo são flores

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -2,28 +2,28 @@ import './Rodape.css'
 
 const Rodape = () => {
     return (
-        <footer className='rodape'>
+        <footer className='footer'>
             <section >
                 <ul className='redes_sociais'>
                     <li>
                         <a href='' target='_blank'>
-                            <img src='/imagens/fb.png' alt='Facebook'/>
+                            <img src='/imagens/fb.png' alt='Facebook' />
                         </a>
                     </li>
                     <li>
                         <a href='' target='_blank'>
-                            <img src='/imagens/tw.png' alt='Twitter'/>
+                            <img src='/imagens/tw.png' alt='Twitter' />
                         </a>
                     </li>
                     <li>
                         <a href='' target='_blank'>
-                        <img src='/imagens/ig.png' alt='Instagram'/>
+                            <img src='/imagens/ig.png' alt='Instagram' />
                         </a>
                     </li>
                 </ul>
             </section>
             <section className='logo'>
-                <img src='/imagens/logo.png' alt='Logo organo'/>
+                <img src='/imagens/logo.png' alt='Logo organo' />
             </section>
             <section className='dev'>Desenvolvido por Alura</section>
 
